25d008bdd1e3631ff211a9a9d7242b4c0473681a
Pavithra M <paveeithra@gmail.com>
2020-06-06 14:32:39 +0530

    Add files via upload

---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+    const cardComponent = robots.map((user,i) => {
+        return <Card key={i} id={robots[i].id} name={robots[i].name} username={robots[i].username} email={robots[i].email} />
+    } )
+    return(
+        <div>
+            {cardComponent}
+        </div>
+    );
+}
+export default CardList;
